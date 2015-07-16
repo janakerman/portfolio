86e8faa170cbd33343146609dcc2c8fa138a610d
Jan Akerman <jakerman@scottlogic.com>
2015-07-16 15:10:33 +0100

    Sets http-server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,45 @@
+'use strict';
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: [
+                'Gruntfile.js',
+                'app/**/*.js'
+            ],
             options: {
-                globals: {
-                    jQuery: true
-                }
+                jshintrc: '.jshintrc'
             }
         },
         watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
+            files: [
+                'app/**/*'
+            ],
+            tasks: ['dev']
+        },
+        'http-server': {
+            dev: {
+                root: '.',
+                port: 8080,
+                host: "0.0.0.0",
+                showDir: true,
+                autoIndex: true,
+                ext: "html",
+                runInBackground: true
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-http-server');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', 'dev');
+
+    grunt.registerTask('dev', ['jshint', 'watch']);
+    // Should start the server
+    grunt.registerTask('dev:serve', ['http-server', 'dev']);
+
 
 };
