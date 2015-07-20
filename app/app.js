36eb7cdad73a92d6fb9ab558f48b71f8c78adb85
Jan Akerman <jakerman@scottlogic.com>
2015-07-20 12:26:50 +0100

    Removes auth module from app module
    This is depended upon from login so there's no need to depend upon
    it directly.

--- app/app.js
+++ app/app.js
@@ -5,14 +5,12 @@ define([
   'uiRouter',
   'structure/structureModule',
   'contacts/contactsModule',
-  'authentication/authenticationModule',
   'login/loginModule',
 ], function(angular) {
   return angular.module('portfolio', [
       'ui.router',
       'portfolio.structure',
       'portfolio.contacts',
-      'portfolio.authentication',
       'portfolio.login'
   ]);
 });
