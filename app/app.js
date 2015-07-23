fbf213240e9123ae92557012ebc5fd05158cdf27
Chris Grant <cgrant@scottlogic.co.uk>
2015-07-23 13:38:41 +0100

    Tidy up login improvements.

--- app/app.js
+++ app/app.js
@@ -30,16 +30,7 @@ define([
           }
         }
       });
-
-      $urlRouterProvider.otherwise(function($injector, $location) {
-        // if (authenticationService.isAuthenticated()) { // TODO: consider when the user is authenticated but not authorised. Get the data from the route and call isAuthorized instead here with the params for this route.
-          return '/contacts';
-        // }
-        // else {
-        //   return '/login';
-        // }
-      });
-
+      $urlRouterProvider.otherwise('/contacts');
     }
   ]);
 
